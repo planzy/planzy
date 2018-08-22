7fc2f2bd95b6bca27846226046a18203e1289701
Erik Cox <erikbcox@gmail.com>
2018-08-21 21:16:16 -0700

    Add google maps

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* globals document */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+/* globals document */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../../src/js/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));

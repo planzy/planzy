f0328c72bb05929a0374aee2b1497d00e597fd50
Erik Cox <erikbcox@gmail.com>
2018-08-23 12:20:37 -0700

    Remove Header, rename Waypoints to Destinations

---
+++ src/js/components/Destinations.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Destinations extends Component {
+  render() {
+    return <div className="destinations" />;
+  }
+}
+
+export default Destinations;
--- src/js/components/Waypoints.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Waypoints extends Component {
-  render() {
-    return <div className="waypoints" />;
-  }
-}
-
-export default Waypoints;

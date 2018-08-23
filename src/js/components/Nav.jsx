9c0daebe3cdc7df7ff34dfb775e2cdab2ce26aef
Joe <jgraythel@gmail.com>
2018-08-23 13:35:40 -0700

    make Nav nice and clean

--- src/js/components/Nav.jsx
+++ src/js/components/Nav.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
 
-const Nav = (props) => {
-  const { addNewTrip, handleChange, username, value, trips, handleChangeTrip } = props;
-  return (
-    <div className="nav" >
-      <div className="nav-dropdown">
-        <select value={value} onChange={(event) => handleChangeTrip(event)}>
-          {trips.map((trip, i) => {
-            return <option key={`trip${i}`} id={trip.id} value={trip.name}>{trip.name}</option>
-            })}
-        </select>
-      </div>
-      <div className="nav-input">
-        <input type="text" onChange={(event) => handleChange(event)} />
-        <button className="btn" onClick={() => addNewTrip()}>Add Trip</button>
-      </div>
-      <div className="user-info">
-        <div className="user-name">{username}</div>
-      </div>
+const Nav = ({
+  addNewTrip,
+  handleChange,
+  username,
+  value,
+  trips,
+  handleChangeTrip,
+}) => (
+  <div className="nav" >
+    <div className="nav-dropdown">
+      <select value={value} onChange={(event) => handleChangeTrip(event)}>
+        {trips.map((trip, i) => {
+          return <option key={`trip${i}`} id={trip.id} value={trip.name}>{trip.name}</option>
+          })}
+      </select>
     </div>
-  );
-}
+    <div className="nav-input">
+      <input type="text" onChange={(event) => handleChange(event)} />
+      <button className="btn" onClick={() => addNewTrip()}>Add Trip</button>
+    </div>
+    <div className="user-info">
+      <div className="user-name">{username}</div>
+    </div>
+  </div>
+);
 
 
 export default Nav;

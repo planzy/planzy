1826c1f02d8f7d3b55b35e970ccf50e2503b568b
Joe Thel <jgraythel@gmail.com>
2018-08-23 10:00:37 -0700

    add simple marker render

--- src/js/components/Trip.jsx
+++ src/js/components/Trip.jsx
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
-import Route from "./Route";
-import Map from "./Map";
+/* globals fetch */
+import React, { Component } from 'react';
+import Route from './Route';
+import Map from './Map';
 
 class Trip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch(`/dest/${this.props.key || 2}`)
+      .then(res => res.json())
+      .then(destinations => this.setState({ destinations }))
+      .catch(console.error);
+  }
+
   render() {
     return (
       <div className="trip">
         <Route />
-        <Map />
+        <Map destinations={this.state.destinations} />
       </div>
     );
   }
